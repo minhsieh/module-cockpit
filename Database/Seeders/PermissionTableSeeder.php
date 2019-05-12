c82f98736a737fedd20e4328bf6f3dbd0d84827e
Min Hsieh <side112358@gmail.com>
2019-05-12 09:47:03 +0800

    修正Seeder完以後，與auth.providers.users.model的model不相符的問題

--- Database/Seeders/PermissionTableSeeder.php
+++ Database/Seeders/PermissionTableSeeder.php
@@ -4,7 +4,6 @@ namespace Modules\Cockpit\Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Eloquent\Model;
-use Modules\Cockpit\Entities\User;
 use Modules\Cockpit\Permission\Models\Role;
 use Modules\Cockpit\Permission\Models\Permission;
 
@@ -19,7 +18,9 @@ class PermissionTableSeeder extends Seeder
     {
         Model::unguard();
 
-        $user = User::where('email','side112358@gmail.com')->first();
+        $model = config('auth.providers.users.model');
+
+        $user = $model::where('email','side112358@gmail.com')->first();
         if(!$user) return;
 
         // Create default admin using permissions
@@ -29,7 +30,7 @@ class PermissionTableSeeder extends Seeder
         $admin_perms[] = Permission::findOrCreate('manage_logs','中控-查看Log','admin');
         $admin_perms[] = Permission::findOrCreate('manage_permissions','中控-管理所有權限','admin');
         $admin_perms[] = Permission::findOrCreate('manage_roles','中控-管理所有角色','admin');
-        
+
         // Create default Team using permissions
         $team_perms[] = Permission::findOrCreate('manage_team_users','管理所有使用者','team');
         $team_perms[] = Permission::findOrCreate('manage_team_roles','管理所有角色','team');
@@ -37,7 +38,7 @@ class PermissionTableSeeder extends Seeder
         //Create Super Admin for MySelf
         $superadmin = Role::findOrCreate('superadmin','超級管理員');
         $team_admin = Role::findOrCreate('team_admin','團隊管理員');
-    
+
 
         $superadmin->givePermissionTo($admin_perms);
         $team_admin->syncPermissions($team_perms);
